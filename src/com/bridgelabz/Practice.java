af06a7de30f06bee3e2caa659c5de092260655ae
Amol Ghotale <amol9.ag@gmail.com>
2022-01-09 00:17:12 +0530

    my first commit

---
+++ src/com/bridgelabz/Practice.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class Practice {
+
+    public static void main(String[] args) {
+	// My first introduction to intelliJ
+        System.out.println("Welcome to java programming");
+    }
+}
